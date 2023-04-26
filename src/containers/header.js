f41b18002eba5a36ddb2aa699135b964de383a99
Krupali15 <124418594+Krupali15@users.noreply.github.com>
2023-04-25 21:02:32 -0400

    Create header.js

---
+++ src/containers/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className="App-header" style={{minHeight:'350px'}}>
+            <span className="HeaderName">"Krupali's Blog"</span>
+            {  /*  <h1 className="App-title">Welcome to React</h1> */}
+        </header>
+    )
+}
+
+export default Header;
